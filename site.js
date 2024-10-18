9c520364632162db53bf63fc65822224553c4571
DomSterling98 <dom.sterling.98@gmail.com>
2024-10-17 22:31:15 -0500

    added secret message

--- site.js
+++ site.js
@@ -6,12 +6,17 @@ const isEvening = hours >= 17 || hours < 4 // is it evening?
 
 let welcomeMessage;
 if (isMorning) {
-    welcomeMessage = "Good morning! Welcome and thank you for visiting.";
+    welcomeMessage = "Good morning! Welcome and thank you for visiting."
 } else if (isAfternoon) {
-    welcomeMessage = "Good afternoon! Welcome and thank you for visiting.";
+    welcomeMessage = "Good afternoon! Welcome and thank you for visiting."
 } else if (isEvening) {
-    welcomeMessage = "Good evening! Welcome and thank you for visiting.";
+    welcomeMessage = "Good evening! Welcome and thank you for visiting."
 }
 
-// Add the message to the page
-document.getElementById('welcome').innerText = welcomeMessage;
+// Add secret message to the page
+document.getElementById('welcome').innerText = welcomeMessage
+
+const secretMessage = "Is this a Zelda reference?"
+localStorage.setItem("It's a secret to everybody.", secretMessage)
+
+console.log('Secret message has been stored in localStorage.')
